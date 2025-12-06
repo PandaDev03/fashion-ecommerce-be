a655db2d1554061c5fc7385cb208c7a361065868
PandaDev03 <daiphucduongvinh203@gmail.com>
2025-12-06 18:12:01 +0700

    feat(brand): Implement API for brand listing and details retrieval

--- src/modules/brand/brand.module.ts
+++ src/modules/brand/brand.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
 import { BrandController } from './brand.controller';
+import { BrandRepository } from './brand.repository';
 import { BrandService } from './brand.service';
+import { Brand } from './entity/brand.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Brand])],
   controllers: [BrandController],
-  providers: [BrandService]
+  providers: [BrandService, BrandRepository],
+  exports: [BrandService, BrandRepository],
 })
 export class BrandModule {}
--- src/modules/brand/brand.service.ts
+++ src/modules/brand/brand.service.ts
@@ -1,4 +1,13 @@
 import { Injectable } from '@nestjs/common';
 
+import { BrandRepository } from './brand.repository';
+import { GetBrandDto } from './dto/get-brand.dto';
+
 @Injectable()
-export class BrandService {}
+export class BrandService {
+  constructor(private readonly brandRepository: BrandRepository) {}
+
+  async findAll(getBrandDto: GetBrandDto) {
+    return await this.brandRepository.findAll(getBrandDto);
+  }
+}
